217b761866b8ae1247130ca693aeefe73e30b132
HenokTG <hena143877tg@gmail.com>
2022-07-24 12:25:55 +0300

    Telegram app ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { ReactNotifications } from "react-notifications-component";
 
 import "./App.css";
@@ -13,12 +13,18 @@ import HomePage from "./pages/Home";
 import ThanksPage from "./pages/Thanks";
 
 const teleObj = window.Telegram.WebApp;
+const webLink =
+  "https://hulugram-fund.herokuapp.com/706699c0-0d8e-41bb-bdbe-a041833b1af7/donate";
 
 function App() {
   useEffect(() => {
+    console.log(teleObj);
     teleObj.ready();
-  }, [])
-  
+    // teleObj.MainButton.text("Main Button");
+    teleObj.MainButton.show();
+    // teleObj.openLink(webLink);
+  }, []);
+
   return (
     <div className={`App`}>
       <Router>
--- src/Bot/bot.js
+++ src/Bot/bot.js
@@ -1,8 +1,15 @@
 const { Telegraf } = require("telegraf");
 
 const BOT_TOKEN = "5524495934:AAFTNTejoN2bk2vSEr5IoK8KKJI12o3ZIgk";
-const webLink = ""
+const webLink =
+  "https://hulugram-fund.herokuapp.com/706699c0-0d8e-41bb-bdbe-a041833b1af7/donate";
 const bot = new Telegraf(BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome to Hulugram Fund"));
+bot.start((ctx) =>
+  ctx.reply("Welcome to Hulugram Fund", {
+    reply_markup: {
+      inline_keyboard: [[{ text: "Hulu Donate", web_app: { url: webLink } }]],
+    },
+  })
+);
 
 bot.launch();
