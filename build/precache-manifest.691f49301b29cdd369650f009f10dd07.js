80eddb0a163d472f3a1b8567a35d26e7e8a2efd3
HenokTG <hena143877tg@gmail.com>
2022-08-09 12:48:39 +0300

    build removed from gitignore

---
+++ build/precache-manifest.691f49301b29cdd369650f009f10dd07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4cd39bf96bd4ec1f202",
+    "url": "/static/css/main.ed1e557f.chunk.css"
+  },
+  {
+    "revision": "f4cd39bf96bd4ec1f202",
+    "url": "/static/js/main.f4cd39bf.chunk.js"
+  },
+  {
+    "revision": "43ae1c72f073a636b989",
+    "url": "/static/css/1.b3387118.chunk.css"
+  },
+  {
+    "revision": "43ae1c72f073a636b989",
+    "url": "/static/js/1.43ae1c72.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e27c8735da98ec6ccdcf12e258b26475",
+    "url": "/static/media/commentAvator.e27c8735.png"
+  },
+  {
+    "revision": "c361623f8e8ceee2891f0d3ff846eee0",
+    "url": "/static/media/organizer.c361623f.jpg"
+  },
+  {
+    "revision": "17dfa51071cdaa1e988926505d0e5bcc",
+    "url": "/static/media/googlePlayNoPadding.17dfa510.jpg"
+  },
+  {
+    "revision": "293d6114a8439dce77383e5b5463a2f4",
+    "url": "/index.html"
+  }
+];
